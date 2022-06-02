43a036320c0eae9bd50ef253ce8c0e04cc47e5b3
UmaMaheshB <maheshuma.bandi@gmail.com>
2022-06-02 23:06:08 +0530

    6/2/22 note creation, classbased views, forms

--- note/urls.py
+++ note/urls.py
@@ -3,6 +3,8 @@ from note.views import *
 
 
 urlpatterns = [
-    path('all/', notes), 
+    path('new/', AddNote.as_view()), 
+    path('all/', notes, name="all_notes"), 
     path('<int:id>/', note_details), 
+    # path('save/', save_note), 
 ]
--- note/views.py
+++ note/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from note.models import Note
+from django.views import View
 
 # Create your views here.
 # def helloworld(request):
@@ -24,11 +25,32 @@ from note.models import Note
 # 	print(html)
 # 	return HttpResponse(html)
 
+class AddNote(View):
+	def get(self, request):
+		return render(request, 'add_note.html')
+
+	def post(self, request):
+		title = request.POST.get("title")
+		description = request.POST.get("description")
+		note = Note(title=title, description=description)
+		note.save()
+		return redirect('all_notes')
+
+# def add_note(request):
+# 	if request.method == "POST":
+# 		title = request.POST.get("title")
+# 		description = request.POST.get("description")
+# 		note = Note(title=title, description=description)
+# 		note.save()
+# 		return redirect('all_notes')
+# 	else:
+# 		return render(request, 'add_note.html')
+
 def notes(request):
 	# notes = ["daily works", "schedule", "meetings details"]
 	notes = Note.objects.all()
 	return render(request, 'notes.html', {"notes": notes})
 
 def note_details(request, id):
-	notes = ["daily works", "schedule", "meetings details"]
-	return HttpResponse(notes[id])
+	note = Note.objects.get(id=id)
+	return render(request, 'note_details.html', {"note": note})
